5cc0c962a572d034d169ca6518673ccec8eed79f
Daniel Dumitrascu <daniel.dumitrascu.dev@gmail.com>
2025-08-07 05:37:56 +0100

    updated the scripts after testing on raspberrry pi

--- setup/scripts/setup_bluetooth.sh
+++ setup/scripts/setup_bluetooth.sh
@@ -2,26 +2,49 @@
 
 BLUETOOTH_USERNAME="daniel"
 BLUETOOTH_CONFIG="/etc/bluetooth/main.conf"
-BLUETOOTH_COMMANDS="power on,discoverable on,pairable on,agent on,default-agent,quit"
-BT_AGENT_SERVICE="/etc/systemd/system/bt-agent.service"
-BT_AGENT_SERVICE_CONTENT="[Unit]\nDescription=Bluetooth Auth Agent\nAfter=bluetooth.service\nPartOf=bluetooth.service\n\n[Service]\nType=simple\nExecStart=/usr/bin/bt-agent -c NoInputNoOutput\n\n[Install]\nWantedBy=bluetooth.target\n"
+BT_AGENT_FILENAME="bt-agent.service"
+BT_AGENT_SERVICE_PATH="/etc/systemd/system/$BT_AGENT_FILENAME"
+BT_AGENT_SERVICE_CONTENT="[Unit]
+Description=Bluetooth Auth Agent
+After=bluetooth.service
+PartOf=bluetooth.service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/bt-agent -c NoInputNoOutput
+
+[Install]
+WantedBy=bluetooth.target
+"
 
 echo " --> Add the user $BLUETOOTH_USERNAME to the bluetooth group, allowing it to access or control Bluetooth devices"
-usermod -a -G bluetooth "$BLUETOOTH_USERNAME"
+sudo usermod -a -G bluetooth "$BLUETOOTH_USERNAME"
 
 echo " --> Append the line Class = 0x41C to the end of the file $BLUETOOTH_CONFIG"
-sed -i '$a Class = 0x41C' "$BLUETOOTH_CONFIG"
+sudo sed -i '$a Class = 0x41C' "$BLUETOOTH_CONFIG"
 
 echo " --> Set the device to stays discoverable indefinitely, which is useful for persistent pairing availability."
-sed -i '$a DiscoverableTimeout = 0' "$BLUETOOTH_CONFIG"
+sudo sed -i '$a DiscoverableTimeout = 0' "$BLUETOOTH_CONFIG"
 
 echo " --> Restart bluetooth module"
-systemctl restart bluetooth
+sudo systemctl restart bluetooth
 
 echo " --> Execute bluetooth commands"
-bluetoothctl <<EOF
-$(echo "$BLUETOOTH_COMMANDS" | tr ',' '\n')
-EOF
+{
+  echo "power on"
+  sleep 1
+  echo "discoverable on"
+  sleep 1
+  echo "pairable on"
+  sleep 1
+  echo "agent on"
+  sleep 1
+  echo "default-agent"
+  sleep 1
+  echo "quit"
+} | bluetoothctl &
+
+sleep 10
 
 echo " --> Start pulseaudio"
 pulseaudio --start
@@ -36,16 +59,18 @@ else
 fi
 
 echo " --> Setup pulseaudio so that it starts at boot"
-systemctl --user enable pulseaudio
+sudo systemctl --user enable pulseaudio
 
-echo " --> Check if the service bt-agent.service exists. If not, then create it"
-if [ ! -f "$BT_AGENT_SERVICE" ]; then
-    echo "Create the service"
-    touch "$BT_AGENT_SERVICE"
-    echo "Adding the service content"
-    echo "$BT_AGENT_SERVICE_CONTENT" > "$BT_AGENT_SERVICE"
-fi
+echo " --> Create the service bt-agent.service"
+touch "$BT_AGENT_FILENAME"  
+  
+echo " --> Adding the service content"
+echo "$BT_AGENT_SERVICE_CONTENT"
+echo "$BT_AGENT_SERVICE_CONTENT" > "$BT_AGENT_FILENAME"
+    
+echo " --> Move service file to the right location"
+sudo mv "$BT_AGENT_FILENAME" "$BT_AGENT_SERVICE_PATH"
 
-echo "Enable and start the bt-agent"
-systemctl enable bt-agent
-systemctl start bt-agent
+echo " --> Enable and start the bt-agent"
+sudo systemctl enable bt-agent
+sudo systemctl start bt-agent
